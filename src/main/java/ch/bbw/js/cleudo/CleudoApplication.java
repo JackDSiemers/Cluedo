70b36b78f430c3de461719bde594651cce0679d7
Jack Siemers <95015162+JackDSiemers@users.noreply.github.com>
2022-09-01 09:05:09 +0200

    initial commit

---
+++ src/main/java/ch/bbw/js/cleudo/CleudoApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.js.cleudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleudoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleudoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/js/cleudo/CleudoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.js.cleudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleudoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
